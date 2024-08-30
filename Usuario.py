9f37e79869c1bc7897ddf18f88991e5cb1d841b1
Alefe <alefefreitassantos@gmail.com>
2024-08-30 13:21:12 -0300

    Pratica tres

---
+++ Usuario.py
@@ -0,0 +1,7 @@
+usuario = input('Insira seu nome de usuario: ')
+senha = input('Insira sua senha: ')
+
+if usuario == 'alefe_santos' and senha == 'estrela123':
+    print('Acesso concedido')
+else:
+    print('Usuario ou senha incorretos') 
